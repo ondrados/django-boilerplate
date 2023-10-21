5a605bd0b19a3f565267d4f5bd7fa26d25dada33
ondrados <ondrejdoskocil@gmail.com>
2023-10-21 19:43:16 +0200

    Update ALLOWED_HOSTS and CORS_ALLOWED_ORIGINS

--- src/app/settings.py
+++ src/app/settings.py
@@ -17,14 +17,9 @@ SECRET_KEY = os.environ.get(
 # SECURITY WARNING: don"t run with debug turned on in production!
 DEBUG = bool(int(os.environ.get("DEBUG", default=0)))
 
-ALLOWED_HOSTS = [
-    "localhost",
-    "127.0.0.1",
-]
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", "localhost,127.0.0.1").split(",")
 
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:8000",
-]
+CORS_ALLOWED_ORIGINS = os.environ.get("CORS_ALLOWED_ORIGINS", "http://localhost:8000").split(",")
 
 SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 
